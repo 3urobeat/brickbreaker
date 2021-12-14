51d5ebc73466d4b7432b32ba39c3622d4b472b86
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-12-15 00:11:22 +0100

    Code improvements
    Co-Authored-By: thePulpo <93628144+thePulpo@users.noreply.github.com>

--- Assets/src/Ball.cs
+++ Assets/src/Ball.cs
@@ -1,29 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
     public float speed;
+    public float maxBounceAngle;
+    public float velocity;
 
     private Rigidbody2D _rigidbody;
     
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, -4f, 0); //Set position to start point
+        transform.position = new Vector3(0, -2.5f, 0); //Set position to start point
 
-        Vector2 force = new Vector2(0, 0);
-        force.x = Random.Range(-1f, 1f);       //get a random number between -1 and 1 to randomize trajectory on start a bit
-        force.y = -1f;
-        
+        Vector2 direction = new Vector2(Random.Range(-1f, 1f), -1f); //get a random number between -1 and 1 to randomize trajectory on start a bit
+
         _rigidbody = gameObject.GetComponent<Rigidbody2D>();
-        _rigidbody.AddForce(force.normalized * speed);
+        _rigidbody.velocity = direction.normalized * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = _rigidbody.velocity.magnitude;
     }
 }
--- Assets/src/Platform.cs
+++ Assets/src/Platform.cs
@@ -7,33 +7,34 @@ public class Platform : MonoBehaviour
 {
     public float speed;
     public float maxBounceAngle;
+    public Transform ball;
     
-    private Rigidbody2D _rigidBody;
-    private Vector2 _direction;
+    private Vector3 _direction = new Vector3();
+    private Vector2 _field;
+    private Ball _ball;
+    private Rigidbody2D _ballRigidbody;
     
     // Start is called before the first frame update
     void Start()
     {
+        _field = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight));
         transform.position = new Vector3(0, -4.25f, 0); //Set position to start point
         
-        _rigidBody = gameObject.GetComponent<Rigidbody2D>(); //assign rigidBody on start
+        _ballRigidbody = ball.GetComponent<Rigidbody2D>();
+        _ball = ball.GetComponent<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _direction = new Vector2(Input.GetAxis("Horizontal"), 0); //Direction of the input
-        
-        _rigidBody.velocity = speed * _direction; //set velocity of rigidBody
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x + Input.GetAxis("Horizontal") * speed, -_field.x + transform.localScale.x / 2, _field.x - transform.localScale.x / 2), transform.position.y); //set velocity of rigidBody
     }
 
     //Handle trajectory change on ball collision (I didn't know how to implement it myself - Credit: https://www.youtube.com/watch?v=RYG8UExRkhA)
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Ball ball = collision.gameObject.GetComponent<Ball>();
-
         //Check if the ball hit
-        if (ball)
+        if (collision.collider.transform == ball)
         {
             Vector3 platformPosition = transform.position;                 //Get position of the platform
             Vector2 collisionPoint   = collision.GetContact(0).point; //Get point where the ball hit the platform
@@ -41,13 +42,12 @@ public class Platform : MonoBehaviour
             float offset = platformPosition.x - collisionPoint.x;     //calculate distance between position and hit to calculate amount of trajectory change
             float width  = collision.otherCollider.bounds.size.x / 2;
 
-            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.GetComponent<Rigidbody2D>().velocity);
+            float currentAngle = Vector2.SignedAngle(Vector2.up, _ballRigidbody.velocity);
             float bounceAngle  = (offset / width) * maxBounceAngle;
             float newAngle     = Mathf.Clamp(currentAngle + bounceAngle, -maxBounceAngle, maxBounceAngle);
 
             Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
-            ball.GetComponent<Rigidbody2D>().velocity =
-                rotation * Vector2.up * ball.GetComponent<Rigidbody2D>().velocity.magnitude;
+            _ballRigidbody.velocity = rotation.normalized * Vector2.up * _ballRigidbody.velocity.magnitude;
         }
     }
 }
