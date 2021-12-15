55c5fd3a88e5f97282a4b7a8db3cca5b1aff8468
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-12-15 03:16:20 +0100

    Added score counter and final screen
    Co-Authored-By: thePulpo <93628144+thePulpo@users.noreply.github.com>

--- Assets/src/Ball.cs
+++ Assets/src/Ball.cs
@@ -2,15 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
+    public Transform gameController;
     public float speed;
     public float maxBounceAngle;
     public float velocity;
+    public GameObject scoreCounter;
+    public GameObject resultPanel;
 
+    public int hits;
+    
     private Rigidbody2D _rigidbody;
+    private BrickGen _brickGen;
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +28,8 @@ public class Ball : MonoBehaviour
 
         _rigidbody = gameObject.GetComponent<Rigidbody2D>();
         _rigidbody.velocity = direction.normalized * speed;
+        
+        _brickGen = gameController.GetComponent<BrickGen>();
     }
 
     // Update is called once per frame
@@ -38,6 +47,31 @@ public class Ball : MonoBehaviour
         if (col.gameObject.tag == "Brick")
         {
             Destroy(col.gameObject); //Destroy brick on hit
+
+            hits++;
+            scoreCounter.GetComponent<Text>().text = "Score: " + hits;
+
+            //Show win screen if player hit all bricks
+            if (hits >= _brickGen.rows * _brickGen.cols)
+            {
+                resultPanel.SetActive(true);
+                resultPanel.GetComponentInChildren<Text>().text = "Win!";
+                
+                scoreCounter.SetActive(false); //hide the ingame score counter
+        
+                Destroy(gameObject); //stop the ball from floating around in the empty void of its existence
+            }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //Only the bottom wall has a trigger for checking for a failure
+        resultPanel.SetActive(true);
+        resultPanel.GetComponentInChildren<Text>().text = "Failure\n\nYour Score: " + hits;
+        
+        scoreCounter.SetActive(false); //hide the ingame score counter
+        
+        Destroy(gameObject); //stop the ball from floating around in the empty void of its existence
     }
 }
